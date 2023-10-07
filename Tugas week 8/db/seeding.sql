d8f07f949bb714fa3a9c35890f1ec7444f1a6126
Fathurahmanalf <113759542+Fathurahmanalf@users.noreply.github.com>
2023-10-07 19:26:53 +0700

    Tugas Homework 8 Rakamin

---
+++ Tugas week 8/db/seeding.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.actor (actor_id, first_name, last_name, last_update)
+VALUES (201, 'Fathurahman', 'AL Faridzi', NOW()),
+       (202, 'Angga', 'Parulian', NOW()),
+       (203, 'Dyas', 'Tri', NOW()),
+       (204, 'Zacky', 'Afif', NOW()),
+       (205, 'Radian', 'Rama', NOW());
+
+
+
